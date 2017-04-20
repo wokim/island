6bc22d708e14d5bc3410a19dd4d0edae061e9e83
Ted Seo <deadlines@gmail.com>
2017-04-20 18:08:18 +0900

    feat: add function to change hostname to ip

--- src/adapters/impl/mongoose-adapter.ts
+++ src/adapters/impl/mongoose-adapter.ts
@@ -1,3 +1,4 @@
+import * as dns from 'dns';
 import * as mongoose from 'mongoose';
 
 import { FatalError, ISLAND } from '../../utils/error';
@@ -24,17 +25,49 @@ export default class MongooseAdapter extends AbstractAdapter<mongoose.Connection
       if (!this.options) throw new FatalError(ISLAND.FATAL.F0025_MISSING_ADAPTER_OPTIONS);
       // Mongoose buffers all the commands until it's connected to the database.
       // But make sure to the case of using a external mongodb connector
-      const connection = mongoose.createConnection(this.options.uri, this.options.connectionOptions);
-      connection.once('open', () => {
-        this._adaptee = connection;
-        connection.removeAllListeners();
-        resolve();
-      });
-      connection.once('error', err => {
-        reject(err);
+      const uri = this.options.uri;
+      const connectionOptions = this.options.connectionOptions;
+      return new Promise((resolve, reject) => {
+        this.dnsLookup(uri)
+        .then(address => {
+          const connection = mongoose.createConnection(address, connectionOptions);
+          connection.once('open', () => {
+            this._adaptee = connection;
+            connection.removeAllListeners();
+            resolve();
+          });
+          connection.once('error', err => {
+            reject(err);
+          });
+        });
       });
     });
   }
 
   public destroy() {}
+
+  private async dnsLookup(uri) {
+    let address = 'mongodb://';
+    let h = uri.split(address)[1];
+    if (h.indexOf('@') > -1) {
+      address += h.split('@')[0] + '@';
+      h = h.split('@')[1];
+    }
+    address += 'HOST';
+    const host = h.split(':')[0];
+    if (h.split(':')[1]) {
+      address += ':' + h.split(':')[1];
+    }
+    // tslint:disable-next-line
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(host)) {
+      return uri;
+    }
+    return await new Promise((resolve, reject) => {
+      dns.lookup(host, (err, ip) => {
+        if (err) return reject(err);
+        address = address.replace('HOST', ip);
+        return resolve(address);
+      });
+    });
+  }
 }
