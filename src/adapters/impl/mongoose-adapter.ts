77528ad42f5916bc7d7d79118f3fe2db687b2758
Ted Seo <deadlines@gmail.com>
2017-04-21 15:16:26 +0900

    fix: Change function to use mongodb-uri

--- src/adapters/impl/mongoose-adapter.ts
+++ src/adapters/impl/mongoose-adapter.ts
@@ -1,3 +1,4 @@
+import * as Bluebird from 'bluebird';
 import * as dns from 'dns';
 import * as mongoose from 'mongoose';
 
@@ -27,18 +28,15 @@ export default class MongooseAdapter extends AbstractAdapter<mongoose.Connection
       // But make sure to the case of using a external mongodb connector
       const uri = this.options.uri;
       const connectionOptions = this.options.connectionOptions;
-      return new Promise((resolve, reject) => {
-        this.dnsLookup(uri)
-        .then(address => {
-          const connection = mongoose.createConnection(address, connectionOptions);
-          connection.once('open', () => {
-            this._adaptee = connection;
-            connection.removeAllListeners();
-            resolve();
-          });
-          connection.once('error', err => {
-            reject(err);
-          });
+      this.dnsLookup(uri).then(address => {
+        const connection = mongoose.createConnection(address, connectionOptions);
+        connection.once('open', () => {
+          this._adaptee = connection;
+          connection.removeAllListeners();
+          resolve();
+        });
+        connection.once('error', err => {
+          reject(err);
         });
       });
     });
@@ -46,27 +44,33 @@ export default class MongooseAdapter extends AbstractAdapter<mongoose.Connection
 
   public destroy() {}
 
+//   private async dnsLookup(uri) {
+//     const mongodbUri = require('mongodb-uri');
+//     const h = mongodbUri.parse(uri);
+//     return await Promise.all(h.hosts.map(async host => {
+//       await this.convert(host.host).then(ip => { host.host = ip; });
+//     })).then(() => {
+//       const address = mongodbUri.format(h);
+//       return address;
+//     });
+//  }
+
   private async dnsLookup(uri) {
-    let address = 'mongodb://';
-    let h = uri.split(address)[1];
-    if (h.indexOf('@') > -1) {
-      address += h.split('@')[0] + '@';
-      h = h.split('@')[1];
-    }
-    address += 'HOST';
-    const host = h.split(':')[0];
-    if (h.split(':')[1]) {
-      address += ':' + h.split(':')[1];
-    }
-    // tslint:disable-next-line
-    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(host)) {
-      return uri;
-    }
+    const mongodbUri = require('mongodb-uri');
+    const h = mongodbUri.parse(uri);
+    return Bluebird.map(h.hosts, (async (host: {host: string}) => {
+      await this.convert(host.host).then((ip: string) => { host.host = ip; });
+    })).then(() => {
+      const address = mongodbUri.format(h);
+      return address;
+    });
+  }
+
+  private async convert(host) {
     return await new Promise((resolve, reject) => {
       dns.lookup(host, (err, ip) => {
         if (err) return reject(err);
-        address = address.replace('HOST', ip);
-        return resolve(address);
+        return resolve(ip);
       });
     });
   }
